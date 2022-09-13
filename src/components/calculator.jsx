e10bf7a52e83243174937bd6ca3b5283fd9c03fa
eternitycodes <matthew.bansford@amalitech.org>
2022-09-12 20:00:20 -0700

    react calculator

---
+++ src/components/calculator.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "../calculator.css";
+import { useState } from 'react';
+
+
+
+function Calculator () {
+    const [result, setResult] = useState('');
+    
+    const handleClick = (event) => {
+        setResult(result.concat(event.target.value))
+    }
+    
+
+    const clearDisplay = () => {
+        setResult('')
+    }
+    
+    const calculate = () => {
+        setResult(eval(result).toString())
+    }
+
+    return (
+        <>
+        <div className="background">
+         <div className='calc'>
+         <input type='text' placeholder='0' id='answer' value={result}/>
+         <input type="button" value='9' className="button" onClick={handleClick} />
+         <input type="button" value='7' className="button"  onClick={handleClick}/>
+         <input type="button" value='8' className="button"  onClick={handleClick}/>
+         <input type="button" value='6' className="button" onClick={handleClick}/>
+         <input type="button" value='5' className="button" onClick={handleClick}/>
+         <input type="button" value='4' className="button" onClick={handleClick}/>
+         <input type="button" value='3' className="button" onClick={handleClick}/>
+         <input type="button" value='2' className="button" onClick={handleClick}/>
+         <input type="button" value='1' className="button" onClick={handleClick}/>
+         <input type="button" value='0' className="button" onClick={handleClick}/>
+
+         <input type="button" value='+' className="button"  onClick={handleClick}/>
+         <input type="button" value='-' className="button" onClick={handleClick}/>
+         <input type="button" value='*' className="button" onClick={handleClick}/>
+         <input type="button" value='/' className="button" onClick={handleClick}/>
+         <input type="button" value='%' className="button" onClick={handleClick}/>
+         <input type="button" value='.' className="button"  onClick={handleClick}/>
+
+         <input type="button" value='Clear' className="button button1" onClick={clearDisplay}/>
+         <input type="button" value='=' className="button button1" onClick={calculate}/>
+        </div>
+      </div>
+        </>
+    )
+}
+
+export default Calculator;
